79528adbe02329398c7d411a68a389c113b88966
michaelgosling <MGosling94@gmail.com>
2018-05-18 13:16:21 -0300

    Getting the hang of widgets and classes.

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,73 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
+import 'dart:core';
 
-void main() => runApp(new MyApp());
+final List<Widget> armorList = [new ListTile(title: new Text("Test")),
+                   new Divider(),
+                   new ListTile(title: new Text("Test")),
+                   new Divider(),
+                   new ListTile(title: new Text("Test")),
+                   new Divider(),
+                   new ListTile(title: new Text("Test"))];
 
-class MyApp extends StatelessWidget {
+var currentList = 0;
+
+final List<Widget> drawerContent = [
+  new Container(child: new Column(
+    children: <Widget>[
+      new ListTile(
+        title: new Text("Armor"),
+        onTap: () {},
+      ),
+      new ListTile(
+        title: new Text("Weapons"),
+        onTap: () {},
+      ),
+      new ListTile (
+        title: new Text("Items"),
+        onTap: () {},
+      ),
+      new Divider(),
+      new ListTile (
+        title: new Text("About"),
+        onTap: () {},
+      )
+    ],
+  ),)
+];
+
+List<Widget> itemListGeneration(newItems){
+  List<Widget> items = [];
+  for (final item in newItems)
+    items.add(item);
+  return items;
+}
+
+class CompendiumApp extends StatefulWidget {
+  @override
+  _HomePageState createState() => new _HomePageState();
+}
+
+class _HomePageState extends State<CompendiumApp> {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: '5e Compendium',
-      theme: new ThemeData(
-        primarySwatch: Colors.black,
-      ),
-      home: new ItemList(),
+        title: "5e Compendium",
+        home: Scaffold(
+            appBar: new AppBar(
+              title: new Text("5e Compendium"),
+            ),
+            drawer: new Drawer(
+              child: new ListView(
+                children: drawerContent,
+              ),
+            ),
+            body: new ListView(
+              children: itemListGeneration(armorList),
+            )
+        )
     );
   }
 }
 
-class ItemList extends StatefulWidget {
-  @override
-  _ItemListState createState() => new _ItemListState();
-}
-
-class _ItemListState extends State<ItemList> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-
-    );
-  }
-}
-
-
+void main() => runApp(new CompendiumApp());
