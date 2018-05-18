5a5b85eed08a0cc3d494bc4ce8624baceee79b5f
michaelgosling <MGosling94@gmail.com>
2018-05-18 08:58:15 -0300

    Getting started

--- lib/main.dart
+++ lib/main.dart
@@ -3,21 +3,12 @@ import 'package:flutter/material.dart';
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: 'Flutter Demo',
+      title: '5e Compendium',
       theme: new ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
-        // counter didn't reset back to zero; the application is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.black,
       ),
       home: new ItemList(),
     );
@@ -25,16 +16,16 @@ class MyApp extends StatelessWidget {
 }
 
 class ItemList extends StatefulWidget {
-
   @override
   _ItemListState createState() => new _ItemListState();
 }
 
 class _ItemListState extends State<ItemList> {
-
   @override
   Widget build(BuildContext context) {
+    return Scaffold(
 
+    );
   }
 }
 
